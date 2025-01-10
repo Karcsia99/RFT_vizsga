1326e7448d79eeda957edd5c09917e3a3576737a
Károly <karcsia99@gmail.com>
2025-01-10 13:15:38 +0100

    Commit

--- AutoAlkatreszRaktar.java
+++ AutoAlkatreszRaktar.java
@@ -27,3 +27,12 @@ private static boolean login() {
         String username = scanner.nextLine();
         System.out.print("Jelszó: ");
         String password = scanner.nextLine();
+        
+        if (users.containsKey(username) && users.get(username).equals(password)) {
+            System.out.println("Sikeres bejelentkezés!");
+            return true;
+        } else {
+            System.out.println("Hibás felhasználónév vagy jelszó.");
+            attempts++;
+        }
+    }
