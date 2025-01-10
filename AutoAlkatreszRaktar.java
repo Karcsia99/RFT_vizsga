1960d915bff08a9e94cd4f2a64ce3fcb613fa0f8
Domcsi-S <domcsisirko@gmail.com>
2025-01-10 21:43:10 +0100

    Update AutoAlkatreszRaktar.java

--- AutoAlkatreszRaktar.java
+++ AutoAlkatreszRaktar.java
@@ -18,7 +18,26 @@ public class AutoAlkatreszRaktar {
             System.out.println("Túl sok sikertelen próbálkozás. Kilépés...");
         }
 }
+private static boolean login() {
+        // GitHub Commit: Added login functionality by Dominika
+        int attempts = 0;
+        while (attempts < 3) {
+            System.out.print("Felhasználónév: ");
+            String username = scanner.nextLine();
+            System.out.print("Jelszó: ");
+            String password = scanner.nextLine();
 
+            if (users.containsKey(username) && users.get(username).equals(password)) {
+                System.out.println("Sikeres bejelentkezés!");
+                return true;
+            } else {
+                System.out.println("Hibás felhasználónév vagy jelszó.");
+                attempts++;
+            }
+        }
+        return false;
+    }
+    
 private static void menu() {
     
     while (true) {
