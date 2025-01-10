a5e6d6d832c133ad0a3f7c6e3ec78bd0fdbd2ec5
Károly <karcsia99@gmail.com>
2025-01-10 13:09:23 +0100

    Commit

--- AutoAlkatreszRaktar.java
+++ AutoAlkatreszRaktar.java
@@ -22,3 +22,8 @@ public class AutoAlkatreszRaktar {
 private static boolean login() {
     // GitHub Commit: Added login functionality by Dominika
     int attempts = 0;
+    while (attempts < 3) {
+        System.out.print("Felhasználónév: ");
+        String username = scanner.nextLine();
+        System.out.print("Jelszó: ");
+        String password = scanner.nextLine();
