973b7a61daec2979d8faa72f8609b8db800ef0a7
Károly <karcsia99@gmail.com>
2025-01-10 13:05:40 +0100

    Commit

--- AutoAlkatreszRaktar.java
+++ AutoAlkatreszRaktar.java
@@ -18,3 +18,7 @@ public class AutoAlkatreszRaktar {
             System.out.println("Túl sok sikertelen próbálkozás. Kilépés...");
         }
 }
+
+private static boolean login() {
+    // GitHub Commit: Added login functionality by Dominika
+    int attempts = 0;
