163d4b80630a7f473e4aaa40fdb941137ce22751
Károly <karcsia99@gmail.com>
2025-01-10 11:50:37 +0100

    Commit

--- AutoAlkatreszRaktar.java
+++ AutoAlkatreszRaktar.java
@@ -1,8 +1,14 @@
 import java.util.*;
 
 public class AutoAlkatreszRaktar {
-    
+
     private static Map<String, String> users = new HashMap<>(); // Felhasználók
     private static List<String> inventory = new ArrayList<>(); // Autóalkatrész-készlet
     private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        users.put("admin", "password123"); // Alapértelmezett felhasználó
+
+        System.out.println("Üdvözöljük az autóalkatrész-készlet-kezelő rendszerben!");
 }
